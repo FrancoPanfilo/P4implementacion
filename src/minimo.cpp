103792f3d8886c9e23c16a8c1dc0fce1b8089106
Alen Rama <alen.rama@fing.edu.uy>
2024-06-17 01:55:26 -0300

    CargaDeDatos

--- src/minimo.cpp
+++ src/minimo.cpp
@@ -1,6 +1,7 @@
-#include "../include/Minimo.h"
-/*
-bool Minimo::verificarMinimo(std::set<ParProdCant> prod)
-{
-    return cantidad <= prod.cantidad;
-} */
+#include "../include/Datatypes/Minimo.h"
+
+bool Minimo::verificarMinimo(std::set<ParProdCant> prod){
+    //return cantidad <= prod.cantidad;
+    bool res;
+    return res;
+}
