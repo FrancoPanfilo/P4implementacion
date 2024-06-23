4d166062342779d8887fee662ab3950fdbcfffc4
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2024-06-23 16:00:19 -0300

    datos comentarios

--- include/controladorComentarios.h
+++ include/controladorComentarios.h
@@ -21,18 +21,18 @@ private:
     ControladorComentarios();
     ~ControladorComentarios();
 
-	map<int, Comentario*> comentarios;
+    map<int, Comentario *> comentarios;
 
-	// Registro de ids
-	int ultimaId;
+    // Registro de ids
+    int ultimaId;
 
-	// Memoria
-	string comentador; // Nickname del usuario ingresando un Comentario
-	string contenido;  // Contenido del comentario
-	int respondiendoA; // Id del comentario al que estamos respondiendo
-	Producto *comentarioSobre; // Referencia al producto correspondiente al comentario
-	
-	//void borrarComentarioRecursivo(int idComentario);
+    // Memoria
+    string comentador;         // Nickname del usuario ingresando un Comentario
+    string contenido;          // Contenido del comentario
+    int respondiendoA;         // Id del comentario al que estamos respondiendo
+    Producto *comentarioSobre; // Referencia al producto correspondiente al comentario
+
+    // void borrarComentarioRecursivo(int idComentario);
 
 public:
     static ControladorComentarios *getInstance();
@@ -46,7 +46,9 @@ public:
     void elegirYBorrarComentario(int idComentario);
 
     // Dejar Comentario
-	void elegirProducto(int codigo);
+    void cargarComentario(DTComentario, string);
+    void cargarRespuesta(DTComentario, int, string);
+    void elegirProducto(int codigo);
     void seleccionarUsuarioCom(string nick);
     void introducirTexto(string contenido);
     set<DTComentario> listarComentarios();
